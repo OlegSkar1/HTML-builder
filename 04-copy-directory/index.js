ff214b1a4d9a87897cfd3d5088baeb8f7f97fd57
Oleg Skarednov <olegskar1@gmail.com>
2022-09-02 19:10:41 +0500

    feat: 04-copy-dir

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,46 @@
+const { pipeline } = require('stream/promises');
+const { createReadStream, createWriteStream } = require('fs');
+const { readdir, mkdir, unlink } = require('fs/promises');
+const { join } = require('path');
 
+const source = join(__dirname, 'files');
+const dist = join(__dirname, 'files-copy');
+
+const copyDir = async (source, dist) => {
+  try {
+    await mkdir(dist, { recursive: true });
+    const readSource = await readdir(source);
+
+    readSource.forEach(async (file) => {
+      const pathToSourceFile = join(source, file);
+      const pathToDistFile = join(dist, file);
+
+      const rs = createReadStream(pathToSourceFile);
+      const ws = createWriteStream(pathToDistFile);
+
+      await pipeline(rs, ws);
+    });
+  } catch (err) {
+    console.log(err.message);
+  }
+
+  trackFiles(source, dist);
+};
+
+const trackFiles = async (source, dist) => {
+  try {
+    const readDist = await readdir(dist);
+    const readSource = await readdir(source);
+
+    readDist.forEach(async (file) => {
+      const pathToDistFile = join(dist, file);
+      if (!readSource.includes(file)) {
+        await unlink(pathToDistFile);
+      }
+    });
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+copyDir(source, dist);
