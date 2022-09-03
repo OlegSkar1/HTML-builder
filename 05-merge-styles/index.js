8080cf589e77fdcb8a9902a56474349418187f02
Oleg Skarednov <olegskar1@gmail.com>
2022-09-03 16:57:34 +0500

    feat: 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,33 @@
+const { join, extname } = require('path');
+const { createReadStream, createWriteStream } = require('fs');
+const { readdir, stat, unlink } = require('fs/promises');
+const { pipeline } = require('stream/promises');
+
+module.exports = mergeFiles;
+
+const mergeFiles = async (source, dist, distFileName) => {
+  source = join(__dirname, source);
+  dist = join(__dirname, dist);
+  const pathToBundle = join(dist, distFileName);
+  try {
+    await stat(pathToBundle);
+    await unlink(pathToBundle);
+    mergeFiles('styles', 'project-dist', 'bundle.css');
+  } catch (error) {
+    const readSource = await readdir(source);
+
+    readSource.forEach(async (file) => {
+      const filePath = join(source, file);
+      const extName = extname(filePath) === '.css';
+      const fileStats = await stat(filePath);
+
+      if (fileStats.isFile() && extName) {
+        const rs = createReadStream(filePath);
+        const ws = createWriteStream(pathToBundle, { flags: 'a' });
+        await pipeline(rs, ws);
+      }
+    });
+  }
+};
+
+mergeFiles('styles', 'project-dist', 'bundle.css');
