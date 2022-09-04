cf29304452a6d737db7bc4bef975f100b1c97f1b
Oleg Skarednov <olegskar1@gmail.com>
2022-09-04 22:19:21 +0500

    feat: copy template.html to index.html

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,7 +1,9 @@
 const copyDir = require('../04-copy-directory');
 const mergeFiles = require('../05-merge-styles');
 const { join } = require('path');
+const { createReadStream, createWriteStream } = require('fs');
 const { mkdir } = require('fs/promises');
+const { pipeline } = require('stream/promises');
 
 const dist = join(__dirname, 'project-dist');
 const sourceAssets = join(__dirname, 'assets');
@@ -13,7 +15,16 @@ const createApp = async () => {
   try {
     await copyDir(sourceAssets, distAssets);
     await mergeFiles(sourceStyles, distStyles);
+
+    const templatePath = join(__dirname, 'template.html');
+    const indexPath = join(dist, 'index.html');
+
+    const rs = createReadStream(templatePath);
+    const ws = createWriteStream(indexPath);
+
+    await pipeline(rs, ws);
   } catch (err) {
+    console.log(err.message);
     await mkdir(dist);
   }
 };
