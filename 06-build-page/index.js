96f4f17092895c3990c38038751757365dddff4d
Oleg Skarednov <olegskar1@gmail.com>
2022-09-04 21:56:08 +0500

    feat:template to index.html

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,8 +1,8 @@
 const copyDir = require('../04-copy-directory');
 const mergeFiles = require('../05-merge-styles');
-const { join } = require('path');
+const { join, basename } = require('path');
 const { createReadStream, createWriteStream } = require('fs');
-const { mkdir } = require('fs/promises');
+const { mkdir, readdir, readFile } = require('fs/promises');
 const { pipeline } = require('stream/promises');
 
 const dist = join(__dirname, 'project-dist');
@@ -21,8 +21,21 @@ const createApp = async () => {
 
     const rs = createReadStream(templatePath);
     const ws = createWriteStream(indexPath);
-
     await pipeline(rs, ws);
+
+    let index = await readFile(indexPath, 'utf-8');
+    const componentsPath = join(__dirname, 'components');
+
+    const components = await readdir(componentsPath);
+    components.forEach(async (component) => {
+      const pathToComponent = join(componentsPath, component);
+      const basenameComponent = basename(pathToComponent, '.html');
+
+      const componentContent = await readFile(pathToComponent);
+
+      index += index.replace(`{{${basenameComponent}}}`, componentContent);
+    });
+    await pipeline(index, ws);
   } catch (err) {
     console.log(err.message);
     await mkdir(dist);
